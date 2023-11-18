7f5594a69d50d1e5f57c4ca6dde7708d4aa4107c
2peng23 <aryayouh@gmail.com>
2023-11-19 03:06:31 +0800

    commit

--- resources/views/default/price.blade.php
+++ resources/views/default/price.blade.php
@@ -10,211 +10,44 @@
             </div>
         </div>
         <div class="row">
+            @php
+                $package = App\Models\Package::all();
+            @endphp
             <!-- Single Table -->
-            <div class="col-lg-4 col-md-12 col-12 wow fadeInLeft">
-                <div class="single-table">
-                    <!-- Table Head -->
-                    <div class="table-head">
-                        <div class="icon">
-                            <i class="icofont icofont-box"></i>
-                        </div>
-                        <h4 class="title">Senior Package</h4>
+            @foreach ($package as $item)
+                <div class="col-lg-4 col-md-12 col-12 wow fadeInDown">
+                    <div class="single-table">
+                        <!-- Table Head -->
+                        <div class="table-head">
+                            <div class="icon">
+                                <i class="icofont icofont-box"></i>
+                            </div>
+                            <div class="d-flex justify-content-between">
+                                <h4 class="title">{{ $item->name }}</h4>
+                                <h4 class="title text-primary">P{{ $item->price }}</h4>
+                            </div>
 
-                    </div>
-                    <!-- Table List -->
-                    <ul class="table-list text-uppercase">
-                        <div class="row">
-                            <div class="col-6">
-                                <li><i class="icofont icofont-ui-check"></i>CBC w/ PC</li>
-                                <li><i class="icofont icofont-ui-check"></i>FBS</li>
-                                <li><i class="icofont icofont-ui-check"></i>BUA</li>
-                                <li><i class="icofont icofont-ui-check"></i>BUN</li>
-                                <li><i class="icofont icofont-ui-check"></i>Treatinine</li>
+                        </div>
+                        <!-- Table List -->
+                        <ul class="table-list text-uppercase">
+                            <div class="row">
+                                @foreach ($item->inclusion as $inc)
+                                    <div class="col-6">
+                                        <li><i class="icofont icofont-ui-check"></i>{{ $inc }}</li>
+                                    </div>
+                                @endforeach
                             </div>
-                            <div class="col-6">
-                                <li><i class="icofont icofont-ui-check"></i>Triglycerides</li>
-                                <li><i class="icofont icofont-ui-check"></i>HDL</li>
-                                <li><i class="icofont icofont-ui-check"></i>LDL</li>
-                                <li><i class="icofont icofont-ui-check"></i>SGOT</li>
-                                <li><i class="icofont icofont-ui-check"></i>SGPT</li>
-                            </div>
+                        </ul>
+                        <div class="table-bottom">
+                            <a class="btn" href="#">Avail</a>
                         </div>
-                    </ul>
-                    <div class="table-bottom">
-                        <a class="btn" href="#">Avail</a>
+                        <!-- Table Bottom -->
                     </div>
-                    <!-- Table Bottom -->
                 </div>
-            </div>
-            <!-- End Single Table-->
-            <!-- Single Table -->
-            <div class="col-lg-4 col-md-12 col-12 wow fadeInUp">
-                <div class="single-table">
-                    <!-- Table Head -->
-                    <div class="table-head">
-                        <div class="icon">
-                            <i class="icofont icofont-box"></i>
-                        </div>
-                        <h4 class="title">Pedia Package</h4>
-                    </div>
-                    <!-- Table List -->
-                    <ul class="table-list text-uppercase">
-                        <li><i class="icofont icofont-ui-check"></i>CBC w/ PC</li>
-                        <li><i class="icofont icofont-ui-check"></i>Urinalysis</li>
-                        <li><i class="icofont icofont-ui-check"></i>Fecalysis</li>
-                    </ul>
-                    <div class="table-bottom">
-                        <a class="btn" href="#">Avail</a>
-                    </div>
-                    <!-- Table Bottom -->
-                </div>
-            </div>
-            <!-- End Single Table-->
-            <!-- Single Table -->
-            <div class="col-lg-4 col-md-12 col-12 wow fadeInRight">
-                <div class="single-table">
-                    <!-- Table Head -->
-                    <div class="table-head">
-                        <div class="icon">
-                            <i class="icofont-box"></i>
-                        </div>
-                        <h4 class="title">Pregnant Package</h4>
-                        {{-- <div class="price">
-                            <p class="amount">$399<span>/ Per Visit</span></p>
-                        </div> --}}
-                    </div>
-                    <!-- Table List -->
-                    <ul class="table-list text-uppercase">
-                        <li><i class="icofont icofont-ui-check"></i>CBC w/ Platelet</li>
-                        <li><i class="icofont icofont-ui-check"></i>Urinalysis</li>
-                        <li><i class="icofont icofont-ui-check"></i>Blood Typing</li>
-                        <li><i class="icofont icofont-ui-check"></i>HbsAg</li>
-                        <li><i class="icofont icofont-ui-check"></i>VRDRL</li>
-                    </ul>
-                    <div class="table-bottom">
-                        <a class="btn" href="#">Avail</a>
-                    </div>
-                    <!-- Table Bottom -->
-                </div>
-            </div>
+            @endforeach
             <!-- End Single Table-->
         </div>
         <button id="more-pack" class="px-3 py-3 rounded bg-primary text-white">Show More</button>
-        <div class="row" id="more-info" style="display: none">
-            <!-- Single Table -->
-            <div class="col-lg-4 col-md-12 col-12 wow fadeInDown">
-                <div class="single-table">
-                    <!-- Table Head -->
-                    <div class="table-head">
-                        <div class="icon">
-                            <i class="icofont icofont-box"></i>
-                        </div>
-                        <h4 class="title">Package 1</h4>
-
-                    </div>
-                    <!-- Table List -->
-                    <ul class="table-list text-uppercase">
-                        <li><i class="icofont icofont-ui-check"></i>CBC w/ PC</li>
-                        <li><i class="icofont icofont-ui-check"></i>Urinalysis</li>
-                        <p class="fw-bolder my-2">Chemistry 10</p>
-                        <li><i class="icofont icofont-ui-check"></i>FBS</li>
-                        <li><i class="icofont icofont-ui-check"></i>Cholesterol</li>
-                        <li><i class="icofont icofont-ui-check"></i>Triglycerides</li>
-                        <li><i class="icofont icofont-ui-check"></i>HDL</li>
-                        <li><i class="icofont icofont-ui-check"></i>LDL</li>
-                        <li><i class="icofont icofont-ui-check"></i>BUA</li>
-                        <li><i class="icofont icofont-ui-check"></i>BUN</li>
-                        <li><i class="icofont icofont-ui-check"></i>Creatinine</li>
-                        <li><i class="icofont icofont-ui-check"></i>SGOT</li>
-                        <li><i class="icofont icofont-ui-check"></i>SGPT</li>
-                        <p class="fw-bolder my-2">Electrolytes</p>
-                        <li><i class="icofont icofont-ui-check"></i>Sodium</li>
-                        <li><i class="icofont icofont-ui-check"></i>Potassium</li>
-                        <li><i class="icofont icofont-ui-check"></i>Chloride</li>
-                    </ul>
-                    <div class="table-bottom">
-                        <a class="btn" href="#">Avail</a>
-                    </div>
-                    <!-- Table Bottom -->
-                </div>
-            </div>
-            <!-- End Single Table-->
-            <!-- Single Table -->
-            <div class="col-lg-4 col-md-12 col-12 wow fadeInDown">
-                <div class="single-table">
-                    <!-- Table Head -->
-                    <div class="table-head">
-                        <div class="icon">
-                            <i class="icofont icofont-box"></i>
-                        </div>
-                        <h4 class="title">Package 2</h4>
-                    </div>
-                    <!-- Table List -->
-                    <ul class="table-list text-uppercase">
-                        <li><i class="icofont icofont-ui-check"></i>CBC w/ PC</li>
-                        <li><i class="icofont icofont-ui-check"></i>Urinalysis</li>
-                        <p class="fw-bolder my-2">Chemistry 10</p>
-                        <li><i class="icofont icofont-ui-check"></i>FBS</li>
-                        <li><i class="icofont icofont-ui-check"></i>Cholesterol</li>
-                        <li><i class="icofont icofont-ui-check"></i>Triglycerides</li>
-                        <li><i class="icofont icofont-ui-check"></i>HDL</li>
-                        <li><i class="icofont icofont-ui-check"></i>LDL</li>
-                        <li><i class="icofont icofont-ui-check"></i>BUA</li>
-                        <li><i class="icofont icofont-ui-check"></i>BUN</li>
-                        <li><i class="icofont icofont-ui-check"></i>Creatinine</li>
-                        <li><i class="icofont icofont-ui-check"></i>SGOT</li>
-                        <li><i class="icofont icofont-ui-check"></i>SGPT</li>
-                    </ul>
-                    <div class="table-bottom">
-                        <a class="btn" href="#">Avail</a>
-                    </div>
-                    <!-- Table Bottom -->
-                </div>
-            </div>
-            <!-- End Single Table-->
-            <!-- Single Table -->
-            <div class="col-lg-4 col-md-12 col-12 wow fadeInDown">
-                <div class="single-table">
-                    <!-- Table Head -->
-                    <div class="table-head">
-                        <div class="icon">
-                            <i class="icofont-box"></i>
-                        </div>
-                        <h4 class="title">Pre-Employment</h4>
-                        {{-- <div class="price">
-                            <p class="amount">$399<span>/ Per Visit</span></p>
-                        </div> --}}
-                    </div>
-                    <!-- Table List -->
-                    <ul class="table-list text-uppercase">
-                        <li><i class="icofont icofont-ui-check"></i>CBC w/ Platelet</li>
-                        <li><i class="icofont icofont-ui-check"></i>Urinalysis</li>
-                        <li><i class="icofont icofont-ui-check"></i>Fecalysis</li>
-                        <li><i class="icofont icofont-ui-check"></i>HbsAg</li>
-                        <li><i class="icofont icofont-ui-check"></i>Chest X-Ray</li>
-                    </ul>
-                    <div class="table-bottom">
-                        <a class="btn" href="#">Avail</a>
-                    </div>
-                    <!-- Table Bottom -->
-                </div>
-            </div>
-            <!-- End Single Table-->
-        </div>
     </div>
 </section>
 <!--/ End Pricing Table -->
-<script>
-    var moreBtn = document.getElementById('more-pack');
-    var info = document.getElementById('more-info');
-
-    moreBtn.addEventListener('click', function() {
-        if (info.style.display === 'flex') {
-            moreBtn.innerHTML = 'Show More'
-            info.style.display = 'none';
-        } else {
-            moreBtn.innerHTML = 'Show Less'
-            info.style.display = 'flex';
-        }
-    });
-</script>
